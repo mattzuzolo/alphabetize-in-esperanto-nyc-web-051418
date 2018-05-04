a9e0f1fa21f0b6ad75e61e26232ccf6b75b67077
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-04 04:55:49 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -10,8 +10,39 @@ def alphabetize(arr)
   
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 esperanto_array = esperanto_alphabet.split("")
-
+final_array = []
+final_element = nil
   
+ 
+    
+    arr.sort
+    
+    
+    
+    arr.sort_by do |element|
+      #arr is array of sentences
+      #element is individual sentences
+      
+      #holds an array of all letters in sentence
+      sentence_letters = []
+      
+      
+      sentence_letters = element.split("")
+      #sentence_letters is an array of each letter/space in the sentence
+      
+      sentence_letters.sort_by do |individual_letters|
+      esperanto_array.index(individual_letters)
+      
+    
+      end
+    end
+end
+
+
+ #https://stackoverflow.com/questions/37990997/how-to-sort-one-array-based-on-another-array-using-ruby
+ 
+ 
+ 
     # new_arr = arr.sort_by do 
       
     #   |element| element.split("").sortby { |
@@ -20,19 +51,19 @@ esperanto_array = esperanto_alphabet.split("")
     #   binding.pry
     # end  
     
-    arr.map do |element|
-      element_split = element.split("")
+    # arr.map do |element|
+    #   element_split = element.split("")
       
-      element_split.sort_by do |element_letters|
+    #   element_split.sort_by do |element_letters|
         
-        
-        #https://stackoverflow.com/questions/37990997/how-to-sort-one-array-based-on-another-array-using-ruby
-        
-       binding.pry
+    #       esperanto_array.find_index(element_letters)
+    #       binding.pry
+           
+    #   end
+
+    
        
-      end
+    # #  final_element = element.join("")
       
       
-    end  
-  
-end
+    # end
